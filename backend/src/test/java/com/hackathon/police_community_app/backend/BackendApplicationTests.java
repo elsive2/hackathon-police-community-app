b43a8df55f8fa8c07753aed6636f406e97a37186
maxim <maximyakovlev228@gmail.com>
2025-03-28 14:43:29 +0500

    BE/Init

---
+++ backend/src/test/java/com/hackathon/police_community_app/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.police_community_app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
