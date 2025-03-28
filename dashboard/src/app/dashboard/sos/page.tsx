7561358a28fbedf4eaf62601b335dc1f4b54d016
Alibek Tauirbaev <featternotving@gmail.com>
2025-03-28 23:20:39 +0500

    table as a separate component

--- dashboard/src/app/dashboard/sos/page.tsx
+++ dashboard/src/app/dashboard/sos/page.tsx
@@ -3,8 +3,7 @@
 import { SOSInterface } from "@/modules/sos/interfaces";
 import { useEffect, useState } from "react";
 import { sosData } from "@/modules/sos/mock";
-import { getStatus } from "@/modules/sos/utils/get-status";
-import { Button, Checkbox, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
+import SosTable from "@/modules/sos/components/SosTable";
 
 const SosPage = () => {
   const [data, setData] = useState<SOSInterface[]>([]);
@@ -21,33 +20,7 @@ const SosPage = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableBody>
-        <TableRow>
-            <TableCell align="left">Местоположение</TableCell>
-            <TableCell align="center">Телефон</TableCell>
-            <TableCell align="center">Дата</TableCell>
-            <TableCell align="center">Статус</TableCell>
-            <TableCell align="center">Кнопки</TableCell>
-          </TableRow>
-          {data.map((row, index) => {
-            return (
-              <TableRow
-                key={row.id}
-                sx={{ cursor: "pointer" }}
-              >
-                <TableCell align="left">{row.location}</TableCell>
-                <TableCell align="center">{row.phone}</TableCell>
-                <TableCell align="center">{row.date}</TableCell>
-                <TableCell align="center">{getStatus(row.status)}</TableCell>
-                <TableCell align="center" ><Button variant="outlined">Принять</Button></TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <SosTable data={data}/>
   );
 };
 
