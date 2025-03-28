d01c99835e90ba4887c1834d3b7d608bf3e5457e
Alibek Tauirbaev <featternotving@gmail.com>
2025-03-28 20:58:52 +0500

    add base table and map items

--- dashboard/src/app/dashboard/sos/page.tsx
+++ dashboard/src/app/dashboard/sos/page.tsx
@@ -4,6 +4,7 @@ import { SOSInterface } from "@/modules/sos/interfaces";
 import { useEffect, useState } from "react";
 import { sosData } from "@/modules/sos/mock";
 import { getStatus } from "@/modules/sos/utils/get-status";
+import { Checkbox, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
 
 const SosPage = () => {
   const [data, setData] = useState<SOSInterface[]>([]);
@@ -20,15 +21,35 @@ const SosPage = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div>
-      {data.map(({ id, phone, status }) => (
-        <div key={id}>
-          <div>{id}</div>
-          <div>{phone}</div>
-          <div>{getStatus(status)}</div>
-        </div>
-      ))}
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {data.map((row, index) => {
+            const labelId = index;
+
+            return (
+              <TableRow
+                hover
+                role="checkbox"
+                key={row.id}
+                sx={{ cursor: "pointer" }}
+              >
+                <TableCell
+                  component="th"
+                  scope="row"
+                  padding="none"
+                >
+                  {row.location}
+                </TableCell>
+                <TableCell align="right">{row.phone}</TableCell>
+                <TableCell align="right">{row.date}</TableCell>
+                <TableCell align="right">{row.status}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
