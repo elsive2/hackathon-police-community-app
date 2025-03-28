3d4add0a4b1bc6f8d753f7aec2febcc15d626da5
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2025-03-28 19:29:34 +0500

    Add mock for sos page

--- dashboard/src/app/dashboard/sos/page.tsx
+++ dashboard/src/app/dashboard/sos/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+
+import { SOSInterface } from "@/modules/sos/interfaces";
+import { useEffect, useState } from "react";
+import { sosData } from "@/modules/sos/mock";
+import { getStatus } from "@/modules/sos/utils/get-status";
+
 const SosPage = () => {
-  return <div>Sos</div>;
+  const [data, setData] = useState<SOSInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setData(sosData);
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      {data.map(({ id, phone, status }) => (
+        <div key={id}>
+          <div>{phone}</div>
+          <div>{getStatus(status)}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SosPage;
