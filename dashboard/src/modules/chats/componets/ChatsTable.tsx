cb2fb3ab3fb65afe1928c85780a25cc15c9c868b
Alibek Tauirbaev <featternotving@gmail.com>
2025-03-29 17:48:21 +0500

    add formatMessage to utils chats

--- dashboard/src/modules/chats/componets/ChatsTable.tsx
+++ dashboard/src/modules/chats/componets/ChatsTable.tsx
@@ -1,7 +1,9 @@
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
 import { ChatInterface } from "../interfaces";
+import { formatMessage } from "../utils/formatMessage";
 
 const ChatsTable = ({ data }: {data: ChatInterface[]}) => {
+
     return (
         <TableContainer component={Paper}>
       <Table>
@@ -19,7 +21,11 @@ const ChatsTable = ({ data }: {data: ChatInterface[]}) => {
                 sx={{ cursor: "pointer" }}
               >
                 <TableCell align="left" >{row.phone}</TableCell>
-                <TableCell align="center">{row.startMessage}</TableCell>
+                <TableCell 
+                align="center" 
+                sx={{whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", maxWidth: "200px", textAlign: "start"}}>
+                    {formatMessage(row.startMessage, 40)}
+                    </TableCell>
                 <TableCell align="center">{row.date}</TableCell>
                 <TableCell align="center">{row.isNew && <Button variant="outlined">Принять</Button>}</TableCell>
               </TableRow>
